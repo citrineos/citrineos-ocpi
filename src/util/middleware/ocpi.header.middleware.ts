ff977d434eaf9acf9cd2c20c13c4808ca15ece30
Elliot Sabitov <>
2024-05-22 09:49:06 -0400

    feat: ensuring that from/to country-code and party-id headers are set in response such that the from values become the to values

--- src/util/middleware/ocpi.header.middleware.ts
+++ src/util/middleware/ocpi.header.middleware.ts
@@ -11,13 +11,23 @@ export class OcpiHeaderMiddleware implements KoaMiddlewareInterface {
     next: (err?: any) => Promise<any>,
   ): Promise<any> {
     try {
+
+      // todo create @Paginated() annotation and set X-Total-Count and X-Limit headers as per pg 28 https://evroaming.org/app/uploads/2021/11/OCPI-2.2.1.pdf
+
       const xRequestId = this.getHeader(context, OcpiHttpHeader.XRequestId);
       const xCorrelationId = this.getHeader(context, OcpiHttpHeader.XCorrelationId);
       context.response.set(OcpiHttpHeader.XRequestId, xRequestId);
       context.response.set(OcpiHttpHeader.XCorrelationId, xCorrelationId);
-      console.log('OcpiHeaderMiddleware before', context);
+
+      const fromCountryCode = this.getHeader(context, OcpiHttpHeader.OcpiFromCountryCode);
+      const fromPartyId = this.getHeader(context, OcpiHttpHeader.OcpiFromPartyId);
+      const toCountryCode = this.getHeader(context, OcpiHttpHeader.OcpiToCountryCode);
+      const toPartyId = this.getHeader(context, OcpiHttpHeader.OcpiToPartyId);
+      context.response.set(OcpiHttpHeader.OcpiFromCountryCode, toCountryCode);
+      context.response.set(OcpiHttpHeader.OcpiFromPartyId, toPartyId);
+      context.response.set(OcpiHttpHeader.OcpiToCountryCode, fromCountryCode);
+      context.response.set(OcpiHttpHeader.OcpiToPartyId, fromPartyId);
       await next();
-      console.log('OcpiHeaderMiddleware after', context);
     } catch (err) {
       console.error('OcpiHeaderMiddleware error', err);
     }
