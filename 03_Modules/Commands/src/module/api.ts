f27f31f3e02b920e90cf4a53d29b2cce865d03ea
leeangioletti <lee.angioletti@sulzer-us.com>
2024-06-05 14:23:14 -0400

    Removed traces of fastify for christian w

--- 03_Modules/Commands/src/module/api.ts
+++ 03_Modules/Commands/src/module/api.ts
@@ -3,32 +3,25 @@
 //
 // SPDX-License-Identifier: Apache 2.0
 
-import { FastifyInstance, FastifyRequest } from 'fastify';
 import { ILogObj, Logger } from 'tslog';
 import {ICommandsModuleApi} from './interface';
 import { CommandsModule } from './module';
-import {
-  AbstractModuleApi,
-} from '@citrineos/base';
 
 /**
  * Server API for the provisioning component.
  */
-export class CommandsModuleApi
-  extends AbstractModuleApi<CommandsModule>
-  implements ICommandsModuleApi {
+export class CommandsModuleApi implements ICommandsModuleApi {
   /**
    * Constructs a new instance of the class.
    *
    * @param {CommandsModule} commandsModule - The Commands module.
-   * @param {FastifyInstance} server - The Fastify server instance.
    * @param {Logger<ILogObj>} [logger] - The logger for logging.
    */
   constructor(
     commandsModule: CommandsModule,
-    server: FastifyInstance,
+    // server: KoaInstance,
     logger?: Logger<ILogObj>,
   ) {
-    super(commandsModule, server, logger);
+
   }
 }
