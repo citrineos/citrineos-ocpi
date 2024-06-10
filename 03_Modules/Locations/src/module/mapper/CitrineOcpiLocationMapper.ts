95d7303842fcb15728a95719fb66549a0daf219f
Venus Yu <venus.yu@s44.team>
2024-06-10 14:17:11 -0400

    ocpi locations module: adding connector + status mappings

--- 03_Modules/Locations/src/module/mapper/CitrineOcpiLocationMapper.ts
+++ 03_Modules/Locations/src/module/mapper/CitrineOcpiLocationMapper.ts
@@ -6,8 +6,11 @@ import { Connector } from '../../../../../Server/src/model/Connector';
 import { GeoLocation } from '../../../../../Server/src/model/GeoLocation';
 import { Location as OcppLocation, ChargingStation, VariableAttribute } from '@citrineos/data/src/layers/sequelize';
 import { EvseStatus } from '../../../../../Server/src/model/EvseStatus';
-import { AttributeEnumType } from '@citrineos/base';
+import { AttributeEnumType, ConnectorEnumType, ConnectorStatusEnumType } from '@citrineos/base';
 import { Capability } from '../../../../../Server/src/model/Capability';
+import { ConnectorType } from '../../../../../Server/src/model/ConnectorType';
+import { ConnectorFormat } from '../../../../../Server/src/model/ConnectorFormat';
+import { PowerType } from '../../../../../Server/src/model/PowerType';
 
 // we need some more locations...
 // as in charging stations at the same location
@@ -69,7 +72,7 @@ export class CitrineOcpiLocationMapper implements IOcpiLocationMapper {
 
     const evse = new Evse();
     evse.uid = chargingStation.id;
-    evse.status = EvseStatus.AVAILABLE; // make dynamic, can get from variable attributes?
+    evse.status = this.getStatus(variableAttributes);
     evse.connectors = Object.keys(connectorVariableAttributesMap).map(id => this.mapToOcpiConnector(Number(id), connectorVariableAttributesMap[id]));
     evse.last_updated = new Date(); // make dynamic, must not be empty
     evse.evse_id = this.getComponent(variableAttributes, 'EVSE', 'EvseId', AttributeEnumType.Actual);
@@ -88,11 +91,13 @@ export class CitrineOcpiLocationMapper implements IOcpiLocationMapper {
   }
 
   mapToOcpiConnector(id: number, variableAttributes: VariableAttribute[]): Connector {
+    const ocppConnectorType = this.getComponent(variableAttributes, 'Connector', 'ConnectorType', AttributeEnumType.Actual);
+
     const connector = new Connector();
     connector.id = String(id);
-    connector.standard = null; // TODO create helper to fill this in
-    connector.format = null; // TODO create helper to fill this in
-    connector.power_type = null // TODO create helper to fill this in;
+    connector.standard = this.getConnectorStandard(ocppConnectorType);
+    connector.format = ConnectorFormat.CABLE; // TODO dynamically determine if CABLE Or SOCKET
+    connector.power_type = this.getConnectorPowerType(ocppConnectorType);
     connector.max_voltage = Number(this.getComponent(variableAttributes, 'EVSE', 'DCVoltage', AttributeEnumType.MaxSet) ?? '0');
     connector.max_amperage = Number(this.getComponent(variableAttributes, 'EVSE', 'DCCurrent', AttributeEnumType.MaxSet) ?? '0');
     connector.max_electric_power = Number(this.getComponent(variableAttributes, 'EVSE', 'Power', AttributeEnumType.MaxSet) ?? '0');
@@ -118,11 +123,86 @@ export class CitrineOcpiLocationMapper implements IOcpiLocationMapper {
     return [];
   }
 
-  getComponent(variableAttributes: VariableAttribute[], component: string, variable: string, attribute: AttributeEnumType): string | null {
+  getComponent(variableAttributes: VariableAttribute[], component: string, variable: string, attribute: AttributeEnumType): string | undefined {
     const matchingVariableAttribute = variableAttributes
       .filter(va => va.component.name === component && va.variable.name === variable && va.type === attribute);
 
-    return matchingVariableAttribute.length > 0 ? matchingVariableAttribute[0].value : null;
+    return matchingVariableAttribute.length > 0 ? matchingVariableAttribute[0].value : undefined;
   }
-  
+
+  getStatus(variableAttributes: VariableAttribute[]): EvseStatus {
+    const parkingBayOccupancy = this.getComponent(variableAttributes, 'BayOccupancySensor', 'Active', AttributeEnumType.Actual);
+
+    if (parkingBayOccupancy === 'true') {
+      return EvseStatus.BLOCKED;
+    }
+
+    const availabilityState = this.getComponent(variableAttributes, 'EVSE', 'AvailabilityState', AttributeEnumType.Actual);
+
+    // TODO add case for REMOVED
+    switch (availabilityState) {
+      case ConnectorStatusEnumType.Occupied:
+        return EvseStatus.CHARGING;
+      case ConnectorStatusEnumType.Available:
+        return EvseStatus.AVAILABLE;
+      case ConnectorStatusEnumType.Unavailable:
+        return EvseStatus.INOPERATIVE;
+      case ConnectorStatusEnumType.Faulted:
+        return EvseStatus.OUTOFORDER;
+      case ConnectorStatusEnumType.Reserved:
+        return EvseStatus.RESERVED;
+      default:
+        return EvseStatus.UNKNOWN;
+    }
+  }
+
+  getConnectorStandard(connectorType: string | undefined): ConnectorType {
+    // TODO determine if mappings are possible for:
+    // s309-1P-32A
+    // sBS1361
+    // sCEE-7-7
+    // Other1PhMax16A
+    // Other1PhOver16A
+    // Other3Ph
+    // Pan
+    // wInductive
+    // wResonant
+    switch (connectorType) {
+      case ConnectorEnumType.cCCS1:
+        return ConnectorType.IEC_62196_T1_COMBO;
+      case ConnectorEnumType.cCCS2:
+        return ConnectorType.IEC_62196_T2_COMBO;
+      case ConnectorEnumType.cG105:
+        return ConnectorType.CHADEMO;
+      case ConnectorEnumType.cTesla:
+        return ConnectorType.TESLA_S
+      case ConnectorEnumType.cType1:
+        return ConnectorType.IEC_62196_T1;
+      case ConnectorEnumType.cType2:
+      case ConnectorEnumType.sType2:
+        return ConnectorType.IEC_62196_T2;
+      case ConnectorEnumType.s309_1P_16A:
+        return ConnectorType.IEC_60309_2_single_16;
+      case ConnectorEnumType.s309_3P_16A:
+        return ConnectorType.IEC_60309_2_three_16;
+      case ConnectorEnumType.s309_3P_32A:
+        return ConnectorType.IEC_60309_2_three_32;
+      case ConnectorEnumType.sType3:
+        return ConnectorType.IEC_62196_T3C;
+      default:
+        // TODO figure out a different default value as needed
+        return ConnectorType.IEC_62196_T1_COMBO;
+    }
+  }  
+
+  getConnectorPowerType(connectorType: string | undefined): PowerType {
+    // TODO include more cases
+    switch (connectorType) {
+      case ConnectorEnumType.cType1:
+        return PowerType.AC_1_PHASE;
+      default:
+        return PowerType.DC;
+    }
+  }
+
 }
