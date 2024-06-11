c2193693ccecc39564a9a0ac33900f65b2fd9e60
Elliot Sabitov <>
2024-06-11 16:15:01 -0400

    chore: removing server old models because they are moved to base

--- Server_OLD/src/model/ProfileType.ts
+++
@@ -1,6 +0,0 @@
-export enum ProfileType {
-  CHEAP = 'CHEAP',
-  FAST = 'FAST',
-  GREEN = 'GREEN',
-  REGULAR = 'REGULAR',
-}
--- Server_OLD/src/model/TariffDimensionType.ts
+++
@@ -1,6 +0,0 @@
-export enum TariffDimensionType {
-  ENERGY = 'ENERGY',
-  FLAT = 'FLAT',
-  PARKING_TIME = 'PARKING_TIME',
-  TIME = 'TIME',
-}
