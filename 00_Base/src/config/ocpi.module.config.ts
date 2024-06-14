2956048319c43fe68d8cac984695b225a69d38c6
Elliot Sabitov <>
2024-06-14 09:18:20 -0400

    chore: removing Constructor and instead using Constructible from typedi

--- 00_Base/src/config/ocpi.module.config.ts
+++ 00_Base/src/config/ocpi.module.config.ts
@@ -1,4 +1,4 @@
-import { Service } from 'typedi';
+import { Service } from "typedi";
 import { Constructable } from "typedi/types/types/constructable.type";
 import { IOcpiModule } from "../model/IOcpiModule";
 
