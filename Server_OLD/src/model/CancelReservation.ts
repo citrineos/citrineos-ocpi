3faef803154196fd5f273ea37eeda6dee5836ceb
Elliot Sabitov <>
2024-06-17 12:27:47 -0400

    chore: removing old model dir

--- Server_OLD/src/model/CancelReservation.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
-import { ResponseUrl } from './ResponseUrl';
-
-export class CancelReservation extends ResponseUrl {
-  @MaxLength(36)
-  @IsString()
-  @IsNotEmpty()
-  reservation_id!: string;
-}
--- Server_OLD/src/model/DayOfWeek.ts
+++
@@ -1,9 +0,0 @@
-export enum DayOfWeek {
-  MONDAY,
-  TUESDAY,
-  WEDNESDAY,
-  THURSDAY,
-  FRIDAY,
-  SATURDAY,
-  SUNDAY,
-}
