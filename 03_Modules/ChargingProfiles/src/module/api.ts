575351708ec0a32f1321396e9e7621ddf90ff15e
leeangioletti <lee.angioletti@sulzer-us.com>
2024-06-17 12:09:54 -0400

    Charging profile changes

---
+++ 03_Modules/ChargingProfiles/src/module/api.ts
@@ -0,0 +1,47 @@
+// Copyright (c) 2023 S44, LLC
+// Copyright Contributors to the CitrineOS Project
+//
+// SPDX-License-Identifier: Apache 2.0
+
+
+import {IChargingProfilesModuleApi} from './interface';
+
+import {Controller, Get, Param, QueryParam} from 'routing-controllers';
+
+import {HttpStatus} from '@citrineos/base';
+import {
+    AsOcpiFunctionalEndpoint,
+    BaseController,
+    generateMockOcpiResponse,
+    ModuleId,
+    ResponseSchema,
+    OcpiResponse, ChargingProfilesService,
+} from '@citrineos/ocpi-base';
+
+import {Service} from 'typedi';
+import {ChargingProfileResponse} from "@citrineos/ocpi-base";
+
+@Controller(`/${ModuleId.ChargingProfiles}`)
+@Service()
+export class ChargingProfilesModuleApi extends BaseController implements IChargingProfilesModuleApi {
+    constructor(readonly service: ChargingProfilesService) {
+        super();
+    }
+
+    @Get('/:sessionId')
+    @AsOcpiFunctionalEndpoint()
+    @ResponseSchema(OcpiResponse<ChargingProfileResponse>, {
+        statusCode: HttpStatus.OK,
+        description: 'Successful response',
+        examples: {
+            success: generateMockOcpiResponse(OcpiResponse<ChargingProfileResponse>),
+        },
+    })
+    async getActiveChargingProfile(
+        @Param('sessionId') sessionId: string,
+        @QueryParam("duration") duration: number,
+        @QueryParam("response_url") responseUrl: string,
+    ): Promise<OcpiResponse<ChargingProfileResponse>> {
+        return this.service.getActiveChargingProfile(sessionId, duration, responseUrl);
+    }
+}
