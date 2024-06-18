b2fb1ff00bf5fc9825933e12ece983e41c4877d0
Elliot Sabitov <>
2024-06-18 09:58:31 -0400

    fix: ClientVersion version number not being set because incorrect key "identifier" was being used

--- 03_Modules/Credentials/src/module/credentials.service.ts
+++ 03_Modules/Credentials/src/module/credentials.service.ts
@@ -235,7 +235,7 @@ export class CredentialsService {
     }
     const clientVersion = ClientVersion.build(
       {
-        identifier: versionNumber,
+        version: versionNumber,
         url: version.url,
         endpoints: versionDetails.data?.endpoints,
       },
--- 03_Modules/Credentials/tests/index.ts
+++ 03_Modules/Credentials/tests/index.ts
@@ -1,14 +1,14 @@
 const _postCredentialsPayload = {
-  token: '321',
-  url: 'http://localhost:8086',
-  roles: [
+  "token": "321",
+  "url": "http://localhost:8086",
+  "roles": [
     {
-      role: 'EMSP',
-      country_code: 'US',
-      party_id: 'MSP',
-      business_details: {
-        name: 'Example Operator',
-        website: 'http://example.com',
+      "role": "EMSP",
+      "country_code": "US",
+      "party_id": "MSS",
+      "business_details": {
+        "name": "Example Operator1",
+        "website": "http://example.com1",
       },
     },
   ],
