1489a165ae4b2bad27391a292ddeeea65580e1b7
danielyoo <daniel.yoo@sulzer-us.com>
2024-06-21 08:47:33 -0400

    refactor: cleaning up session mapper

--- 00_Base/src/mapper/session.mapper.ts
+++ 00_Base/src/mapper/session.mapper.ts
@@ -1,6 +1,11 @@
 import { Service } from 'typedi';
 import { Session } from '../model/Session';
-import { MeasurandEnumType, MeterValueType, TransactionEventEnumType } from '../../../../citrineos-core/00_Base';
+import {
+  MeasurandEnumType,
+  MeterValueType,
+  TransactionEventEnumType,
+  TransactionEventRequest,
+} from '../../../../citrineos-core/00_Base';
 import { TokenType } from '../model/TokenType';
 import { AuthMethod } from '../model/AuthMethod';
 import { SessionStatus } from '../model/SessionStatus';
@@ -10,20 +15,19 @@ import { CdrDimensionType } from '../model/CdrDimensionType';
 
 @Service()
 export class SessionMapper {
-  constructor() {
-  }
+  constructor() {}
 
-  public mapTransactionToSession(fromCountryCode: string,
-                                 fromPartyId: string,
-                                 toCountryCode: string,
-                                 toPartyId: string, transaction: Transaction): Session {
-    const startEvent = transaction.transactionEvents?.find(
-      (event) => event.eventType === TransactionEventEnumType.Started,
+  public mapTransactionToSession(
+    fromCountryCode: string,
+    fromPartyId: string,
+    toCountryCode: string,
+    toPartyId: string,
+    transaction: Transaction,
+    locationId: string,
+  ): Session {
+    const [startEvent, endEvent] = this.getStartAndEndEvents(
+      transaction.transactionEvents,
     );
-    const endEvent = transaction.transactionEvents?.find(
-      (event) => event.eventType === TransactionEventEnumType.Ended,
-    );
-
     return {
       country_code: toCountryCode,
       party_id: toPartyId,
@@ -31,22 +35,13 @@ export class SessionMapper {
       start_date_time: startEvent ? new Date(startEvent.timestamp) : null,
       end_date_time: endEvent ? new Date(endEvent.timestamp) : null,
       kwh: transaction.totalKwh || 0,
-      cdr_token: {
-        // TODO: Token Type needs to be derived from somewhere and based on this, uid value would be chosen
-        // APP_USER UID is provided by MSP. If during RFID, CPO would use this value to identify the token on their scanner
-        uid: "MSP_TOKEN_UID",
-        type: TokenType.OTHER,
-        // TODO: All three below needs to be somehow provided from MSP
-        contract_id: "MSP_CONTRACT_ID",
-        country_code: fromCountryCode,
-        party_id: fromPartyId,
-      },
+      cdr_token: this.createCdrToken(fromCountryCode, fromPartyId),
       // Defaulting to WHITELIST, otherwise Auth support / reservation support needs to be added with MSP
       auth_method: AuthMethod.WHITELIST,
-      location_id: transaction.stationId,
-      evse_uid: "evse-uid", // TODO: Figure out EVSE UID Mapping
+      location_id: locationId,
+      evse_uid: this.getEvseUid(transaction),
       connector_id: String(transaction.evse?.connectorId),
-      currency: "USD", // TODO: Determine currency from configuration
+      currency: 'USD', // TODO: Determine currency from configuration
       charging_periods: this.getChargingPeriods(transaction.meterValues),
       status: endEvent ? SessionStatus.COMPLETED : SessionStatus.ACTIVE,
       last_updated: new Date(),
@@ -55,6 +50,40 @@ export class SessionMapper {
       total_cost: null,
       meter_id: null,
     };
+  }
+
+  private getStartAndEndEvents(
+    transactionEvents: TransactionEventRequest[] = [],
+  ): [
+    TransactionEventRequest | undefined,
+    TransactionEventRequest | undefined,
+  ] {
+    return [
+      transactionEvents.find(
+        (event) => event.eventType === TransactionEventEnumType.Started,
+      ),
+      transactionEvents.find(
+        (event) => event.eventType === TransactionEventEnumType.Ended,
+      ),
+    ];
+  }
+
+  private createCdrToken(mspCountryCode: string, mspPartyId: string) {
+    return {
+      // TODO: Token Type needs to be derived from somewhere and based on this, uid value would be chosen
+      // APP_USER UID is provided by MSP. If during RFID, CPO would use this value to identify the token on their scanner
+      uid: 'MSP_TOKEN_UID',
+      type: TokenType.OTHER,
+      // TODO: Contract ID needs to be provided from MSP
+      contract_id: 'MSP_CONTRACT_ID',
+      country_code: mspCountryCode,
+      party_id: mspPartyId,
+    };
+  }
+
+  private getEvseUid(transaction: Transaction): string {
+    // TODO: Figure out EVSE UID Mapping
+    return 'evse-uid';
   }
 
   private getChargingPeriods(
@@ -69,7 +98,9 @@ export class SessionMapper {
         })),
         tariff_id: null,
       }))
-      .sort((a, b) => (a.start_date_time < b.start_date_time ? -1 : 1));
+      .sort(
+        (a, b) => a.start_date_time.getTime() - b.start_date_time.getTime(),
+      );
   }
 
   private mapMeasurandToCdrDimensionType(
