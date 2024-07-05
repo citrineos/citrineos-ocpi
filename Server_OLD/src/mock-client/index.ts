b41e135a99c72f99beef35ff425f0187de3c5402
thanaParis <thana.paris@s44.team>
2024-07-05 13:05:05 -0400

    removing Server_OLD

--- Server_OLD/src/mock-client/index.ts
+++
@@ -1,8 +0,0 @@
-import 'reflect-metadata';
-import { Container } from 'typedi';
-import { useContainer } from 'routing-controllers';
-import { EmspServer } from './emsp.server';
-
-useContainer(Container);
-const server = Container.get(EmspServer);
-console.log('initialized mock emsp server', server);
