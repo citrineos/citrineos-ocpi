dd17ff717a861d71158f58f5f60bcc3df97a3133
leeangioletti <lee.angioletti@sulzer-us.com>
2024-07-10 10:16:33 -0400

    Fixed tokens seeder

--- seeders/20240619140739-base-tokens.ts
+++ seeders/20240619140739-base-tokens.ts
@@ -1,12 +1,19 @@
 'use strict';
 
-import { QueryInterface } from 'sequelize';
+import { QueryInterface, QueryOptions } from 'sequelize';
 import {
   OcpiSequelizeInstance,
   OcpiServerConfig,
   OcpiTokensMapper,
   TokenDTO,
 } from '@citrineos/ocpi-base';
+import {
+  AdditionalInfo,
+  Authorization,
+  IdToken,
+  IdTokenInfo,
+} from '../../citrineos-core/01_Data';
+import { AdditionalGeoLocation } from '@citrineos/ocpi-base/dist/model/AdditionalGeoLocation';
 
 const _sequelize = new OcpiSequelizeInstance(new OcpiServerConfig()); // needed to init models
 
@@ -15,54 +22,201 @@ module.exports = {
   up: async (_queryInterface: QueryInterface) => {
     const token1 = {
       country_code: 'US',
-      party_id: 'S44',
-      uid: '123e4567-e89b-12d3-a456-426614174000',
-      type: 'RFID',
+      party_id: 'MSP',
+      uid: '001',
+      type: 'APP_USER',
       contract_id: 'contract_001',
       visual_number: 'VIS001',
-      issuer: 'Issuer1',
+      issuer: 'Issuer001',
       group_id: 'group_001',
       valid: true,
       whitelist: 'ALWAYS',
       language: 'EN',
-      default_profile_type: 'default',
-      energy_contract: null,
+      default_profile_type: 'premium',
+      energy_contract: {
+        supplier_name: 'Supplier001',
+        contract_id: 'contract_001',
+      },
       last_updated: new Date(),
-      createdAt: new Date(),
-      updatedAt: new Date(),
     };
 
-    const token2 = {
-      country_code: 'US',
-      party_id: 'XYZ',
-      uid: '987e6543-e21b-65d4-b789-123456789012',
-      type: 'APP_USER',
-      contract_id: 'contract_002',
-      visual_number: 'VIS002',
-      issuer: 'Issuer2',
-      group_id: 'group_002',
-      valid: false,
-      whitelist: 'ALWAYS',
-      language: 'EN',
-      default_profile_type: 'premium',
-      energy_contract: null,
-      last_updated: new Date(),
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
-    const ocppAuth1 = OcpiTokensMapper.mapOcpiTokenToOcppAuthorization(
+    const ocppAuth = OcpiTokensMapper.mapOcpiTokenToOcppAuthorization(
       token1 as TokenDTO,
     );
-    const ocppAuth2 = OcpiTokensMapper.mapOcpiTokenToOcppAuthorization(
-      token2 as TokenDTO,
+
+    const additionalInfoRecord: any = await _queryInterface.bulkInsert(
+      'AdditionalInfos',
+      [
+        {
+          ...ocppAuth.idToken.additionalInfo![0],
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {
+        returning: true,
+        ignoreDuplicates: true,
+      } as QueryOptions,
     );
 
-    // TODO fix seeders after changing ocpi tokens mapper
-    // await ocppAuth1.save();
-    // await ocppAuth2.save();
+    const idTokenRecord: any = await _queryInterface.bulkInsert(
+      'IdTokens',
+      [
+        {
+          idToken: ocppAuth.idToken.idToken,
+          type: ocppAuth.idToken.type,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {
+        returning: true,
+        ignoreDuplicates: true,
+      } as QueryOptions,
+    );
+
+    const groupIdTokenRecord: any = await _queryInterface.bulkInsert(
+      'IdTokens',
+      [
+        {
+          ...ocppAuth.idTokenInfo?.groupIdToken,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {
+        returning: true,
+        ignoreDuplicates: true,
+      } as QueryOptions,
+    );
+
+    if (!groupIdTokenRecord || groupIdTokenRecord.length === 0) {
+      groupIdTokenRecord[0] = (
+        await IdToken.findOne({
+          where: {
+            idToken: ocppAuth.idTokenInfo?.groupIdToken?.idToken,
+            type: ocppAuth.idTokenInfo?.groupIdToken?.type,
+          },
+        })
+      )?.dataValues;
+    }
+
+    const idTokenInfoRecord: any = await _queryInterface.bulkInsert(
+      'IdTokenInfos',
+      [
+        {
+          status: ocppAuth.idTokenInfo?.status,
+          cacheExpiryDateTime: ocppAuth.idTokenInfo?.cacheExpiryDateTime,
+          chargingPriority: ocppAuth.idTokenInfo?.chargingPriority,
+          language1: ocppAuth.idTokenInfo?.language1,
+          language2: ocppAuth.idTokenInfo?.language2,
+          personalMessage: ocppAuth.idTokenInfo?.personalMessage,
+          groupIdTokenId: groupIdTokenRecord[0].id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {
+        returning: true,
+        ignoreDuplicates: true,
+      } as QueryOptions,
+    );
+
+    if (!idTokenRecord || idTokenRecord.length === 0) {
+      idTokenRecord[0] = (
+        await IdToken.findOne({
+          where: {
+            idToken: ocppAuth.idToken.idToken,
+            type: ocppAuth.idToken.type,
+          },
+        })
+      )?.dataValues;
+    }
+
+    if (!additionalInfoRecord || additionalInfoRecord.length === 0) {
+      additionalInfoRecord[0] = (
+        await AdditionalInfo.findOne({
+          where: {
+            additionalIdToken:
+              ocppAuth.idToken.additionalInfo![0].additionalIdToken,
+            type: ocppAuth.idToken.additionalInfo![0].type,
+          },
+        })
+      )?.dataValues;
+    }
+
+    const idTokenAdditionalInfosRecord: any = await _queryInterface.bulkInsert(
+      'IdTokenAdditionalInfos',
+      [
+        {
+          idTokenId: idTokenRecord[0].id,
+          additionalInfoId: additionalInfoRecord[0].id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {
+        returning: true,
+        ignoreDuplicates: true,
+      } as QueryOptions,
+    );
+
+    const authorizationRecord: any = await _queryInterface.bulkInsert(
+      'Authorizations',
+      [
+        {
+          idTokenId: idTokenRecord[0].id,
+          idTokenInfoId: idTokenInfoRecord[0].id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {
+        returning: true,
+        ignoreDuplicates: true,
+      } as QueryOptions,
+    );
+
+    if (!authorizationRecord || authorizationRecord.length === 0) {
+      authorizationRecord[0] = (
+        await Authorization.findOne({
+          where: {
+            idTokenId: idTokenRecord[0].id,
+            idTokenInfoId: idTokenInfoRecord[0].id,
+          },
+        })
+      )?.dataValues;
+    }
+
+    const ocpiTokenRecord: any = await _queryInterface.bulkInsert(
+      'OcpiTokens',
+      [
+        {
+          authorization_id: authorizationRecord[0].id,
+          country_code: token1.country_code,
+          party_id: token1.party_id,
+          type: token1.type,
+          visual_number: token1.visual_number,
+          issuer: token1.issuer,
+          whitelist: token1.whitelist,
+          default_profile_type: token1.default_profile_type,
+          energy_contract: JSON.stringify(token1.energy_contract),
+          last_updated: token1.last_updated,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {
+        returning: true,
+        ignoreDuplicates: true,
+      } as QueryOptions,
+    );
   },
 
   down: async (queryInterface: QueryInterface) => {
-    await queryInterface.bulkDelete('Tokens', {}, {});
+    await queryInterface.bulkDelete('OcpiTokens', {}, {});
+    await queryInterface.bulkDelete('Authorizations', {}, {
+      cascade: true,
+    } as QueryOptions);
   },
 };
