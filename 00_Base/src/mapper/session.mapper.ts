c13ed7d89373e952c498addccee2ab522e30f56b
leeangioletti <lee.angioletti@sulzer-us.com>
2024-07-11 16:03:56 -0400

    Added fix for missing dimension in second charging period

--- 00_Base/src/mapper/session.mapper.ts
+++ 00_Base/src/mapper/session.mapper.ts
@@ -199,12 +199,12 @@ export class SessionMapper {
               type: CdrDimensionType.ENERGY_IMPORT,
               volume: sampledValue.value,
             });
-            const previousEnergy =
+            const previousEnergyImport =
               this.getEnergyImportForMeterValue(previousMeterValue);
-            if (previousEnergy) {
+            if (previousEnergyImport !== undefined) {
               cdrDimensions.push({
                 type: CdrDimensionType.ENERGY,
-                volume: sampledValue.value - previousEnergy,
+                volume: sampledValue.value - previousEnergyImport,
               });
             }
           }
