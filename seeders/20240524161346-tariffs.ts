dfb8aa9e34b125fd9d88490b01290186004ba0f4
Elliot Sabitov <>
2024-07-24 15:54:41 -0400

    chore: lint-fix chore: fixing versions chore: fixing fastify swagger version issue if missed in some places

--- seeders/20240524161346-tariffs.ts
+++ seeders/20240524161346-tariffs.ts
@@ -1,5 +1,5 @@
 'use strict';
-import {QueryInterface, QueryOptions} from 'sequelize';
+import { QueryInterface, QueryOptions } from 'sequelize';
 
 import 'reflect-metadata';
 
@@ -7,51 +7,51 @@ import 'reflect-metadata';
 export = {
   up: async (queryInterface: QueryInterface) => {
     const tariffRecord: any = await queryInterface.bulkInsert(
-        'Tariffs',
-        [
-          {
-            stationId: 1,
-            currency: 'USD',
-            pricePerKwh: 0,
-            pricePerMin: 0.0125,
-            pricePerSession: 1,
-            createdAt: new Date(),
-            updatedAt: new Date(),
-          },
-        ],
+      'Tariffs',
+      [
         {
-          returning: true,
-          ignoreDuplicates: true,
-        } as QueryOptions,
+          stationId: 1,
+          currency: 'USD',
+          pricePerKwh: 0,
+          pricePerMin: 0.0125,
+          pricePerSession: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {
+        returning: true,
+        ignoreDuplicates: true,
+      } as QueryOptions,
     );
 
-      await queryInterface.bulkInsert(
-          'OcpiTariffs',
-          [
-              {
-                  countryCode: "US",
-                  partyId: "CPO",
-                  coreTariffId: tariffRecord[0].id,
-                  createdAt: new Date(),
-                  updatedAt: new Date(),
-              },
-          ],
-          {
-              returning: true,
-              ignoreDuplicates: true,
-          } as QueryOptions,
-      );
+    await queryInterface.bulkInsert(
+      'OcpiTariffs',
+      [
+        {
+          countryCode: 'US',
+          partyId: 'CPO',
+          coreTariffId: tariffRecord[0].id,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {
+        returning: true,
+        ignoreDuplicates: true,
+      } as QueryOptions,
+    );
   },
 
   down: async (queryInterface: QueryInterface) => {
-      await queryInterface.bulkDelete('OcpiTariffs', {}, {
-          truncate: true,
-          cascade: true,
-      } as QueryOptions);
+    await queryInterface.bulkDelete('OcpiTariffs', {}, {
+      truncate: true,
+      cascade: true,
+    } as QueryOptions);
 
-      await queryInterface.bulkDelete('Tariffs', {}, {
-          truncate: true,
-          cascade: true,
-      } as QueryOptions);
+    await queryInterface.bulkDelete('Tariffs', {}, {
+      truncate: true,
+      cascade: true,
+    } as QueryOptions);
   },
 };
