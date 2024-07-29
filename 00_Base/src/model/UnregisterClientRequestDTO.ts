9b97b8a5cf830ba748ae1c36351d6c0dc6c4f084
Elliot Sabitov <>
2024-07-29 14:57:47 -0400

    chore: removing unnecessary

--- 00_Base/src/model/UnregisterClientRequestDTO.ts
+++ 00_Base/src/model/UnregisterClientRequestDTO.ts
@@ -1,26 +1,21 @@
-import { Column, DataType } from 'sequelize-typescript';
 import { IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class UnregisterClientRequestDTO {
-  @Column(DataType.STRING(3))
   @IsString()
   @IsNotEmpty()
   @Length(3, 3)
   serverPartyId!: string;
 
-  @Column(DataType.STRING(2))
   @IsString()
   @IsNotEmpty()
   @Length(2, 2)
   serverCountryCode!: string;
 
-  @Column(DataType.STRING(3))
   @IsString()
   @IsNotEmpty()
   @Length(3, 3)
   clientPartyId!: string;
 
-  @Column(DataType.STRING(2))
   @IsString()
   @IsNotEmpty()
   @Length(2, 2)
