fc043f6d2289e60ad71c234f2c6d4e8d09b47958
Zihe Cheng <lydiazcheng@users.noreply.github.com>
2024-08-15 18:13:44 -0400

    ocpi-reservation: use ServerConfig instead

--- 00_Base/src/repository/OcpiReservationRepository.ts
+++ 00_Base/src/repository/OcpiReservationRepository.ts
@@ -1,6 +1,5 @@
 import { Service } from 'typedi';
 import { SequelizeRepository } from '@citrineos/data';
-import { OcpiServerConfig } from '../config/ocpi.server.config';
 import { ILogObj, Logger } from 'tslog';
 import { OcpiSequelizeInstance } from '../util/sequelize';
 import { OcpiNamespace } from '../util/ocpi.namespace';
@@ -9,18 +8,19 @@ import {
   OcpiReservation,
   OcpiReservationProps,
 } from '../model/OcpiReservation';
+import { ServerConfig } from '../config/ServerConfig';
 
 @Service()
 export class OcpiReservationRepository extends SequelizeRepository<OcpiReservation> {
   logger: Logger<ILogObj>;
 
   constructor(
-    ocpiSystemConfig: OcpiServerConfig,
+    systemConfig: ServerConfig,
     logger: Logger<ILogObj>,
     ocpiSequelizeInstance: OcpiSequelizeInstance,
   ) {
     super(
-      ocpiSystemConfig as SystemConfig,
+      systemConfig as SystemConfig,
       OcpiNamespace.OcpiReservation,
       logger,
       ocpiSequelizeInstance.sequelize,
