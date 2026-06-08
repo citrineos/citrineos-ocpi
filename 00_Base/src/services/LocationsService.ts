f20a9140ba6b63b1fe5b87e4ccd6db8db09e4c68
florianne <coudert.florianne.69@gmail.com>
2026-06-08 08:28:40 +0200

    feat: reuse

--- 00_Base/src/services/LocationsService.ts
+++ 00_Base/src/services/LocationsService.ts
@@ -331,7 +331,6 @@ export class LocationsService {
         undefined,
         partnerRow.awsSecretCertificateArn,
       );
-      console.log('resp', resp);
 
       for (const item of (resp as any).data) {
         processedLocations++;
