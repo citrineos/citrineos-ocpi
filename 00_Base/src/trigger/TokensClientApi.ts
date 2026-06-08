31e74fb2404b067a97d4e6de51b2f25620e8aac2
florianne <coudert.florianne.69@gmail.com>
2026-06-08 12:24:05 +0200

    fix: type params

--- 00_Base/src/trigger/TokensClientApi.ts
+++ 00_Base/src/trigger/TokensClientApi.ts
@@ -71,9 +71,8 @@ export class TokensClientApi extends BaseClientApi {
     awsSecretCertificateArn?: string | null,
   ): Promise<AuthorizationInfoResponse> {
     const path = `${tokenId}/authorize`;
-    const otherParams: Record<string, string> | undefined = tokenType && {
-      type: tokenType,
-    };
+    const tokenTypeParam = tokenType ? { type: tokenType } : undefined;
+
     return this.request(
       fromCountryCode,
       fromPartyId,
@@ -86,7 +85,7 @@ export class TokensClientApi extends BaseClientApi {
       `${this.getUrl(partnerProfile)}/${path}`,
       body,
       undefined,
-      otherParams,
+      tokenTypeParam,
       undefined,
       awsSecretCertificateArn ?? undefined,
     );
