68685869761d7972f43b5d31aad603806a0d9ae0
tleflond <thibaut.leflond@hyxos.com>
2026-07-16 16:56:12 +0200

    cpo callback

--- 00_Base/src/util/ocppCommandHandlers/OCPP2_0_1_CommandHandler.ts
+++ 00_Base/src/util/ocppCommandHandlers/OCPP2_0_1_CommandHandler.ts
@@ -51,7 +51,7 @@ export class OCPP2_0_1_CommandHandler extends OCPPCommandHandler {
     queryParameters.params['tenantId'] = tenantPartner.tenant!.id!;
     queryParameters.params['callbackUrl'] =
       this.config.commands.ocpiBaseUrl +
-      `/emsp/2.2.1/commands/callback/${tenantPartner.id}/${this.supportedVersion}/${CommandType.START_SESSION}/${commandId}`;
+      `/cpo/2.2.1/commands/callback/${tenantPartner.id}/${this.supportedVersion}/${CommandType.START_SESSION}/${commandId}`;
     options.queryParameters = queryParameters;
 
     const sequenceResponse = await this.ocpiGraphqlClient.request<
